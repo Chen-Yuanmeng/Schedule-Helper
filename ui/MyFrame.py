4311c49dd029676d17a73234ed594d75a0bcf73c
cyqm <2784841400@qq.com>
2023-09-10 08:41:47 +0800

    add basic Frame class

---
+++ ui/MyFrame.py
@@ -0,0 +1,13 @@
+import wx
+from DisplayConfig import DISPLAY_SCALE
+
+
+class MyFrame(wx.Frame):
+    def __init__(self, title, width=0, height=0, parent=None, id=-1, style=wx.DEFAULT_FRAME_STYLE):
+        if width == 0 or height == 0:
+            super().__init__(parent, id, title, size=wx.DefaultSize, style=style)
+        else:
+            super().__init__(parent, id, title, size=(width * DISPLAY_SCALE, height * DISPLAY_SCALE), style=style)
+        self.panel = wx.Panel(self)
+        ico = wx.Icon('../ScheduleHelper.ico')
+        self.SetIcon(ico)
